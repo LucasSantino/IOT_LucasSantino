22299d8b6178d78973f5406b90fe513654f5f598
LucasSantino <lsantino541@gmail.com>
2025-02-13 20:17:06 -0300

    Update exer1_centralAlarme.ino

--- Projeto1/exer1_centralAlarme.ino
+++ Projeto1/exer1_centralAlarme.ino
@@ -1,53 +1,56 @@
 // Definição das variáveis (pinos)
-#define BUTTON_LIGA 0
-#define BARREIRA_VERMELHA 1
-#define WINDOW_SENSOR1 2
-#define WINDOW_SENSOR2 3
-#define SALA_SENSOR 4
-#define KITCHEN_SENSOR 5
-#define QUINTAL_SENSOR 6
-#define CORRIDOR_SENSOR 7
-#define SIRENE 8
+#define ON_BUTTON_PIN 0  //define troca um umero por uma palavra
+#define BAR_SENSOR_PIN 1
+#define WINDOW1_SENSOR_PIN 2
+#define WINDOW2_SENSOR_PIN 3
+#define ROOM_SENSOR_PIN 4
+#define KITCHEN_SENSOR_PIN 5
+#define GARAGE_SENSOR_PIN 6
+#define HALL_SENSOR_PIN 7
+#define ALARM_PIN 8
+
+bool onButton = 0, bar_sensor = 0, window1_sensor = 0;
+bool window2_sensor = 0, room_sensor = 0, kitchen_sensor = 0;
+bool garage_sensor = 0 , hall_sensor = 0, alarm = 0;
+
 
 
 // Configuração
 void setup() {
     // Configuração dos sensores como entrada
-    pinMode(BUTTON_LIGA, INPUT);
-    pinMode(BARREIRA_VERMELHA, INPUT);
-    pinMode(WINDOW_SENSOR1, INPUT);
-    pinMode(WINDOW_SENSOR2, INPUT);
-    pinMode(SALA_SENSOR, INPUT);
-    pinMode(KITCHEN_SENSOR, INPUT);
-    pinMode(QUINTAL_SENSOR, INPUT);
-    pinMode(CORRIDOR_SENSOR, INPUT);
+    pinMode(ON_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(BAR_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(WINDOW1_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(WINDOW2_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(ROOM_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(KITCHEN_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(GARAGE_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(HALL_SENSOR_PIN_SENSOR, INPUT_PULLUP);
+    pinMode(ALARM_PIN, INPUT_PULLUP);
     
-
-    pinMode(SIRENE, OUTPUT); // Configuração da sirene como saída
-
-    
-    digitalWrite(SIRENE, LOW); //  sirene inicia desligada
 }
 
 //Logica
 void loop() {
-    // Verifica se o sistema está ligado
-    if (digitalRead(BUTTON_LIGA) == HIGH) {
-        
-        // Verifica as condições para acionar a sirene
-        bool barreiraInterrompida = (digitalRead(BARREIRA_VERMELHA) == LOW);
-        bool janelaViolada = (digitalRead(WINDOW_SENSOR1) == LOW) || (digitalRead(WINDOW_SENSOR2) == LOW);
-        bool movimentoDetectado = (digitalRead(SALA_SENSOR) == HIGH) || (digitalRead(KITCHEN_SENSOR) == HIGH) || 
-                                  (digitalRead(QUINTAL_SENSOR) == HIGH) || (digitalRead(CORRIDOR_SENSOR) == HIGH);
-        
-        // Se alguma condição for verdadeira, ativa a sirene
-        if (barreiraInterrompida || janelaViolada || movimentoDetectado) {
-            digitalWrite(SIRENE, HIGH); // Liga a sirene
-        } else {
-            digitalWrite(SIRENE, LOW); // Desliga a sirene
+    onButton = digitalRead(ON_BUTTON_PIN);   // digitalRead = usada para ler o valor de um pino  
+    bar_sensor = digitalRead(BAR_SENSOR_PIN);
+    window1_sensor = digitalRead(WINDOW1_SENSOR_PIN);
+    window2_sensor = digitalRead(WINDOW1_SENSOR_PIN);
+    room_sensor =  digitalRead(ROOM_SENSOR_PIN);
+    kitchen_sensor = digitalRead(KITCHEN_SENSOR_PIN);
+    garage_sensor = digitalRead(GARAGE_SENSOR_PIN);
+    hall_sensor = digitalRead(HALL_SENSOR_PIN);
+   digitalWrite(ALARM_PIN, alarm);  /// digitalWrite = enviar/escrever um valor HIGH (alto) ou LOW (baixo) para um pino digital do Arduino,
+
+    if(onButton){
+        if( !bar_sensor || !window1_sensor || !window2_sensor ||    /// ! esclamação siginifica falso. || siginifica totalmente igual
+            !room_sensor || !kitchen_sensor || !hall_sensor || !garage_sensor){
+
+            alarm = 1;
         }
-    } else {
-        // Se o botão de ligar estiver desligado, mantém a sirene desativada
-        digitalWrite(SIRENE, LOW);
+        else{
+            alarm = 0;
+        }
     }
+
 }
