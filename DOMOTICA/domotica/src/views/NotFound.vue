e343ea415b0218838cbcf5c34eea79d8e15bcef3
LucasSantino <lsantino541@gmail.com>
2025-02-27 19:54:47 -0300

    Update NotFound.vue

--- DOMOTICA/domotica/src/views/NotFound.vue
+++ DOMOTICA/domotica/src/views/NotFound.vue
@@ -6,7 +6,8 @@
 
 <template>
      <h1>Page not found 😒 !!!</h1>
-     <img src ="https://media.licdn.com/dms/image/v2/C4D12AQGgCdNFB-tZKA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1609432060313?e=2147483647&v=beta&t=_qaNgL1pjWiqjKjvFna7eVQahdF7yRCtaWqYbEduA_Q", alt ="">
+     <img src="https://media.licdn.com/dms/image/v2/C4D12AQGgCdNFB-tZKA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1609432060313?e=2147483647&v=beta&t=_qaNgL1pjWiqjKjvFna7eVQahdF7yRCtaWqYbEduA_Q" alt="">
+
 </template>
 
 <style scoped lang="scss">
