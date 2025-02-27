2327da61aceecebb1d49b57a46284a3db2c8eb9b
LucasSantino <lsantino541@gmail.com>
2025-02-27 20:49:48 -0300

    Update Devices.vue

--- DOMOTICA/domotica/src/views/Devices.vue
+++ DOMOTICA/domotica/src/views/Devices.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
-    import { Environment } from '@/models/devices';
+    import { Device, Environment } from '@/models/devices';
     import { reactive, ref } from 'vue';
 
-    const sala: Environment = {
-        name: 'Sala de Estar',
-        devices: []
-    }
+    const environments: Array<Environment> = reactive([]);
+  
+
+    const ar: Device = reactive(new Device());
+    ar.name = 'Ar condicionado';
+    ar.color = '#3266a8'
+    
+
+    const tv: Device = reactive(new Device());
+    tv.name = 'Smart TV';
+    tv.color = '#a5c949';
+    tv.state = true;
+
+    const iluminacao: Device = reactive(new Device());
+    iluminacao.name = 'Iluminação';
+    iluminacao.color = '#4d0d75';
+    
+    const sala: Environment = reactive(new Environment());
+    sala.name = 'Sala';
+    sala.devices = [ar,tv,iluminacao];
+
+    environments.push(sala);
+
+
 
     
+
 </script>
 
 <template>
      <h1>Devices page!!!</h1>
+     {{ environments }}
 </template>
 
 <style scoped lang="scss">
