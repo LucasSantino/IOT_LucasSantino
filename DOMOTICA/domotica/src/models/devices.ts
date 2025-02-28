0091cd858563ece9435f2b1c3dcb9519af47f229
LucasSantino <lsantino541@gmail.com>
2025-02-27 22:18:07 -0300

    update

--- DOMOTICA/domotica/src/models/devices.ts
+++ DOMOTICA/domotica/src/models/devices.ts
@@ -3,7 +3,7 @@ export class Device {
     name: string = "";
     state: boolean = false;
     color: string = "#827d85";
-    image:;
+    icon: string = "device";
 }
 
 export class Environment {
--- DOMOTICA/domotica/src/views/Devices.vue
+++ DOMOTICA/domotica/src/views/Devices.vue
@@ -7,27 +7,29 @@
 
     const ar: Device = reactive(new Device());
     ar.name = 'Ar condicionado';
-    ar.color = '#3266a8'
+    ar.color = '#3266a8';
+    ar.icon = 'mode_fan';
     
 
     const tv: Device = reactive(new Device());
     tv.name = 'Smart TV';
     tv.color = '#a5c949';
     tv.state = true;
+    tv.icon = 'tv';
 
     const iluminacao: Device = reactive(new Device());
     iluminacao.name = 'Iluminação';
     iluminacao.color = '#4d0d75';
+    iluminacao.icon = 'light';
     
     const sala: Environment = reactive(new Environment());
     sala.name = 'Sala';
     sala.devices = [ar,tv,iluminacao];
 
+
     environments.push(sala);
 
 
-
-    
 
 </script>
 
@@ -35,8 +37,19 @@
     <main class="flex flex-column text-center justify-content-center align-items-center">
         <h1>Devices page!!!</h1>
         <section class="environments flex-column border-round-sm">
-            <div class="device">
+            <div class="device" v-for="(environment, env_id) in environments" :key="env_id">
+                <h3>{{ environment.name }}</h3>
+                <div v-for="(device, dev_id) in environment.devices" :key="dev_id">
+                    <section>
+                        <h5>{{ device.name }}</h5>
+                        <button>ON</button>
+                        <button>OFF</button>
+                        
+                    </section>
+
+                </div>
                 
+
             </div>
 
         </section>
