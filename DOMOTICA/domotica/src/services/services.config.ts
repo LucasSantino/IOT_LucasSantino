2e5734ea56a084cfbdbdcf8729e2dc7a39b6f8b8
LucasSantino <lsantino541@gmail.com>
2025-04-03 22:40:42 -0300

    aula 03-04-25

---
+++ DOMOTICA/domotica/src/services/services.config.ts
@@ -0,0 +1,9 @@
+import axios, {type AxiosResponse} from "axios";
+export const CONTENTFUL_CDN_UrL = 'https://cdn.contentful.com';
+export const CONTENTFUL_API_UrL = 'https://api.contentful.com';
+
+export const getCdnAxios = ()=> {
+    const createdAxios = axios.create({
+
+    });
+}
