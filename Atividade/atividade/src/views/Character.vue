4f99aaed5890d25d689dae987ab6139279fc5acc
LucasSantino <lsantino541@gmail.com>
2025-04-13 15:38:52 -0300

    update: background imagem

--- Atividade/atividade/src/views/Character.vue
+++ Atividade/atividade/src/views/Character.vue
@@ -3,12 +3,11 @@ import { useCharacterStore } from '@/stores/characterStore';
 import CharacterComponent from '@/components/CharacterComponente.vue';
 
 const store = useCharacterStore();
-
 </script>
 
 <template>
   <main class="flex flex-column text-center justify-content-center align-items-center">
-    <h1>Personagens: Star wars!</h1>
+    <h1>Personagens: Star Wars!</h1>
     
     <section class="spaces flex flex-wrap justify-content-center">
       <div v-for="(person, index) in store.spaces[0]?.persons" :key="index">
@@ -27,12 +26,30 @@ const store = useCharacterStore();
 main {
   width: 100vw;
   min-height: 100vh;
+  background-image: url('@/images/starwars_wallpaper.jpg'); 
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 2rem;
+  color: white;
 
   .spaces {
     width: 90vw;
     min-height: 95vh;
-    background-color: rgb(73, 67, 67);
+    background-color: rgba(0, 0, 0, 0.4); // fundo escuro translúcido
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 1rem;
+    border-radius: 10px;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+    text-shadow: 1px 1px 4px black;
+  }
+
+  h2 {
+    color: #ffd700;
+    text-shadow: 1px 1px 2px black;
   }
 }
 </style>
