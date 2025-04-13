1d7547df53c20a470364cf8c4ac947348149bfb9
LucasSantino <lsantino541@gmail.com>
2025-04-13 18:11:51 -0300

    Update CharacterComponente.vue

--- Atividade/atividade/src/components/CharacterComponente.vue
+++ Atividade/atividade/src/components/CharacterComponente.vue
@@ -21,46 +21,24 @@ const handleDelete = () => {
 </script>
 
 <template>
-  <section class="card" :class="{ expanded: expanded }" @click="toggleExpanded">      
+  <section class="card" :class="{ expanded: expanded }" @click="toggleExpanded">
     <h5>{{ props.character.name }}</h5>
 
     <div v-if="expanded">
-  <p><strong>Nome:</strong> {{ props.character.name }}</p>
-  <p><strong>Ano de nascimento:</strong> {{ props.character.birth_year }}</p>
-  <p><strong>Altura:</strong> {{ props.character.height }} cm</p>
-  <p><strong>Peso:</strong> {{ props.character.mass }} kg</p>
-  <p><strong>Cor do cabelo:</strong> {{ props.character.hair_color }}</p>
-  <p><strong>Cor da pele:</strong> {{ props.character.skin_color }}</p>
-  <p><strong>Cor dos olhos:</strong> {{ props.character.eye_color }}</p>
-  <p><strong>Gênero:</strong> {{ props.character.gender }}</p>
-  <p><strong>Planeta Natal:</strong> {{ props.character.homeworld }}</p>
+      <p><strong>Altura:</strong> {{ props.character.height }}</p>
+      <p><strong>Peso:</strong> {{ props.character.mass }}</p>
+      <p><strong>Cor do cabelo:</strong> {{ props.character.hair_color }}</p>
+      <p><strong>Cor da pele:</strong> {{ props.character.skin_color }}</p>
+      <p><strong>Cor dos olhos:</strong> {{ props.character.eye_color }}</p>
+      <p><strong>Ano de nascimento:</strong> {{ props.character.birth_year }}</p>
+      <p><strong>Gênero:</strong> {{ props.character.gender }}</p>
+      <p><strong>Planeta natal:</strong> {{ props.character.homeworld }}</p>
+      <p><strong>Espécies:</strong> {{ props.character.species.join(', ') }}</p>
+      <p><strong>Veículos:</strong> {{ props.character.vehicles.join(', ') }}</p>
+      <p><strong>Naves:</strong> {{ props.character.starships.join(', ') }}</p>
+      <p><strong>Filmes:</strong> {{ props.character.films.join(', ') }}</p>
+    </div>
 
-  <p><strong>Filmes:</strong>
-    <ul>
-      <li v-for="(film, index) in props.character.films" :key="index">{{ film }}</li>
-    </ul>
-  </p>
-
-  <p><strong>Espécies:</strong>
-    <ul>
-      <li v-for="(specie, index) in props.character.species" :key="index">{{ specie }}</li>
-    </ul>
-  </p>
-
-  <p><strong>Veículos:</strong>
-    <ul>
-      <li v-for="(vehicle, index) in props.character.vehicles" :key="index">{{ vehicle }}</li>
-    </ul>
-  </p>
-
-  <p><strong>Naves:</strong>
-    <ul>
-      <li v-for="(starship, index) in props.character.starships" :key="index">{{ starship }}</li>
-    </ul>
-  </p>
-</div>
-
-    
     <div v-else class="hint">Clique para ver mais</div>
 
     <div class="delete-btn" v-if="props.showButtons" @click.stop="handleDelete">
@@ -73,7 +51,8 @@ const handleDelete = () => {
 .card {
   background-color: rgba(46, 46, 46, 0.75);
   border: 1px solid #555;
-  width: 12rem;
+  width: 100%;
+  max-width: 180px;
   min-height: 10rem;
   margin: 0.5rem;
   padding: 1rem;
@@ -84,6 +63,7 @@ const handleDelete = () => {
   transition: all 0.4s ease;
   color: #f0f0f0;
   overflow: hidden;
+  flex: 1 1 calc(16.66% - 1rem); /* Máximo 6 por linha */
 
   &:hover {
     transform: scale(1.02);
@@ -91,7 +71,7 @@ const handleDelete = () => {
   }
 
   &.expanded {
-    width: 16rem;
+    max-width: 220px;
     min-height: 14rem;
   }
 
